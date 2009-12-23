756b8f6547d8187a763930bdb561b36e29f9d438
mbrenn <mbrenn@depon.net>
2009-12-23 19:22:53 +0100

    Stylecop

--- src/EnvironmentHelper.cs
+++ src/EnvironmentHelper.cs
@@ -12,11 +12,8 @@
 namespace BurnSystems
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Reflection;
     using System.IO;
+    using System.Reflection;
 
     /// <summary>
     /// This helper class offers some method to determine environment information
