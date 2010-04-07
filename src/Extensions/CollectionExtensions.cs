c6cc7a614e7c4758780cce5464a26324dfc668f0
mbrenn <mbrenn@depon.net>
2010-04-07 22:41:09 +0200

    Added collection helper

---
+++ src/Extensions/CollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CollectionExtensions.cs" company="Martin Brenn">
+//     Alle Rechte vorbehalten. 
+// 
+//     Die Inhalte dieser Datei sind ebenfalls automatisch unter 
+//     der AGPL lizenziert. 
+//     http://www.fsf.org/licensing/licenses/agpl-3.0.html
+//     Weitere Informationen: http://de.wikipedia.org/wiki/AGPL
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BurnSystems.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// These collection extensions are used to ease the use
+    /// collector class
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Sets a key-value pair in the dictionary
+        /// </summary>
+        /// <typeparam name="T">Type of the key</typeparam>
+        /// <typeparam name="W">Type of the value</typeparam>
+        /// <param name="dict">Dictionary to be modified</param>
+        /// <param name="key">Key to be set</param>
+        /// <param name="value">Value, which shall be assigned to key</param>
+        /// <returns>The same dictionary. </returns>
+        public static Dictionary<T, W> With<T, W>(
+            this Dictionary<T,W> dict, 
+            T key, 
+            W value)
+        {
+            dict[key] = value;
+            return dict;
+        }
+    }
+}
