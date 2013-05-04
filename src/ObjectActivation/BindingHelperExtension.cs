3c42f5f7aafe67dba494f7374a94517c588666ac
Martin Brenn <brenn@depon.net>
2013-05-05 00:36:24 +0200

    Container is being used much better

--- src/ObjectActivation/BindingHelperExtension.cs
+++ src/ObjectActivation/BindingHelperExtension.cs
@@ -66,7 +66,7 @@ namespace BurnSystems.ObjectActivation
                     (container, innerMost, enablers) =>
                     {
                         var result = Activator.CreateInstance(typeof(T));
-                        InstanceBuilder.AddPropertyAssignmentsByReflection(result, container);
+                        InstanceBuilder.AddPropertyAssignmentsByReflection(result, innerMost);
 
                         return result;
                     };
