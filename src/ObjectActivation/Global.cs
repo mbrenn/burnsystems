6e197ebe41465a7894f39f3d9be954aed5f97fad
Martin Brenn <brenn@depon.net>
2014-03-19 09:23:37 +0100

    ObjectActivation/Global.cs is added. Contains a general storage for ActivationContainer

---
+++ src/ObjectActivation/Global.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BurnSystems.ObjectActivation
+{
+    /// <summary>
+    /// Stores intances of activation container, which may be used in global scope and 
+    /// can be accessed via global variables.
+    /// </summary>
+    public class Global
+    {
+        /// <summary>
+        /// Stores the activation container on application scope
+        /// </summary>
+        private static ActivationContainer application;
+
+        /// <summary>
+        /// Returns the activation container on application scope. 
+        /// The variable has to be initialized via InitForApplication()
+        /// </summary>
+        public static ActivationContainer Application
+        {
+            get
+            {
+                if (application == null)
+                {
+                    throw new InvalidOperationException("Application has not been initialized via InitForApplication()");
+                }
+
+                return application;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the activation container on application scope
+        /// </summary>
+        public static void InitForApplication()
+        {
+            if (application != null)
+            {
+                application = new ActivationContainer("Global.Application");
+            }
+        }
+    }
+}
