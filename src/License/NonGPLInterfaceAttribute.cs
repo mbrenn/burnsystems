46ed4ceff120e0896911db3426d864a5f7a2580d
Martin Brenn <brenn@depon.net>
2014-10-22 22:13:10 -0400

    Added the NonGPLInterfaceAttribute

---
+++ src/License/NonGPLInterfaceAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSystems.License
+{
+    /// <summary>
+    /// Defines that components, implementing the given interface, does not to satisfy 
+    /// the GPL licence, even if the product itself is under GPL licence.
+    /// This attribute does not have any other purpose and will not affect the 
+    /// runtime
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
+    public class NonGPLInterfaceAttribute : Attribute
+    {
+    }
+}
