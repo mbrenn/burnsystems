0c9657a3b5577ed0225c5b51144230b4edd0d5c6
Martin Brenn <brenn@depon.net>
2019-12-30 22:34:13 +0100

    Introduced helper methods

--- src/BurnSystems/Logging/LogExtensions.cs
+++ src/BurnSystems/Logging/LogExtensions.cs
@@ -24,6 +24,60 @@ namespace BurnSystems.Logging
             logger.Log(message);
         }
 
+        public static void Log(this ILogger logger, LogLevel logLevel, string messageText, string category, int value, string unit)
+        {
+            var message = new LogMetricMessage<int>
+            {
+                LogLevel = logLevel, 
+                Category = category,
+                Message = messageText, 
+                Value = value,
+                Unit = unit
+            };
+
+            logger.Log(message);
+        }
+
+        public static void Log(this ILogger logger, LogLevel logLevel, string messageText, int value, string unit)
+        {
+            var message = new LogMetricMessage<int>
+            {
+                LogLevel = logLevel,
+                Message = messageText,
+                Value = value,
+                Unit = unit
+            };
+
+            logger.Log(message);
+        }
+
+        public static void Log(this ILogger logger, LogLevel logLevel, string messageText, string category, double value, string unit)
+        {
+            var message = new LogMetricMessage<double>
+            {
+                LogLevel = logLevel,
+                Category = category,
+                Message = messageText,
+                Value = value,
+                Unit = unit
+            };
+
+            logger.Log(message);
+        }
+
+        public static void Log(this ILogger logger, LogLevel logLevel, string messageText, double value, string unit)
+        {
+            var message = new LogMetricMessage<double>
+            {
+                LogLevel = logLevel,
+                Message = messageText,
+                Value = value,
+                Unit = unit
+            };
+
+            logger.Log(message);
+        }
+
         public static void Log(this ILogger logger, LogLevel logLevel, string message)
         {
             logger.Log(logLevel, message, string.Empty);
@@ -57,6 +111,66 @@ namespace BurnSystems.Logging
         public static void Error(this ILogger logger, string message, string category = "")
         {
             logger.Log(LogLevel.Error, message, category);
+        }
+
+        public static void Trace(this ILogger logger, string message, int value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Trace, message, category, value, unit);
+        }
+
+        public static void Debug(this ILogger logger, string message, int value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Debug, message, category, value, unit);
+        }
+
+        public static void Info(this ILogger logger, string message, int value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Info, message, category, value, unit);
+        }
+
+        public static void Fatal(this ILogger logger, string message, int value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Fatal, message, category, value, unit);
+        }
+
+        public static void Warn(this ILogger logger, string message, int value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Warn, message, category, value, unit);
+        }
+
+        public static void Error(this ILogger logger, string message, int value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Error, message, category, value, unit);
+        }
+
+        public static void Trace(this ILogger logger, string message, double value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Trace, message, category, value, unit);
+        }
+
+        public static void Debug(this ILogger logger, string message, double value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Debug, message, category, value, unit);
+        }
+
+        public static void Info(this ILogger logger, string message, double value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Info, message, category, value, unit);
+        }
+
+        public static void Fatal(this ILogger logger, string message, double value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Fatal, message, category, value, unit);
+        }
+
+        public static void Warn(this ILogger logger, string message, double value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Warn, message, category, value, unit);
+        }
+
+        public static void Error(this ILogger logger, string message, double value, string unit, string category = "")
+        {
+            logger.Log(LogLevel.Error, message, category, value, unit);
         }
     }
 }
