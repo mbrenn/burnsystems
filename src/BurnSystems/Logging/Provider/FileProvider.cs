9e24419796df5639a890ab5fb601a8f6be49332c
Martin Brenn <brenn@depon.net>
2020-07-23 21:18:07 +0200

    Creates new filenames, if the currently one cannot be used

--- src/BurnSystems/Logging/Provider/FileProvider.cs
+++ src/BurnSystems/Logging/Provider/FileProvider.cs
@@ -11,7 +11,7 @@ namespace BurnSystems.Logging.Provider
         private readonly object _syncObject = new object();
 
         private StreamWriter? _file;
-
+        
         public FileProvider(string filePath, bool createNew = false)
         {
             if (string.IsNullOrEmpty(filePath)) throw new ArgumentNullException(nameof(filePath));
@@ -30,16 +30,39 @@ namespace BurnSystems.Logging.Provider
             {
                 if (_file == null)
                 {
-                    _file = new StreamWriter(_filePath, !_createNew);
+                    var createTry = 0;
+                    while (createTry < 100 && _file == null)
+                    {
+                        try
+                        {
+                            var fileCore = Path.GetFileNameWithoutExtension(_filePath);
+                            var fileExtension = Path.GetExtension(_filePath);
+                            var directoryPath = Path.GetDirectoryName(_filePath);
+                            var number = createTry == 0 ? string.Empty : "." + createTry;
+
+                            var fileName = Path.Combine(directoryPath, fileCore + number + fileExtension);
+                            _file = new StreamWriter(fileName, !_createNew);
+                        }
+                        catch
+                        {
+                            createTry++;
+                        }
+                    }
+                }
+
+                if (_file == null)
+                {
+                    throw new InvalidOperationException("File could not be created");
                 }
 
                 var timePassed = DateTime.Now - TheLog.TimeCreated;
 
                 if (logMessage is ILogMetricMessage logMetricMessage)
-                {   _file.WriteLine(
-                    $"{DateTime.Now};{timePassed.TotalSeconds.ToString("n3", CultureInfo.InvariantCulture)};" +
-                    $"{logMessage.LogLevel.ToString().PaddingRight(Logger.MaxLengthLogLevel)};{logMessage.Category};" +
-                    $"{logMessage.Message};{logMetricMessage.ValueText};{logMetricMessage.Unit}");
+                {
+                    _file.WriteLine(
+                        $"{DateTime.Now};{timePassed.TotalSeconds.ToString("n3", CultureInfo.InvariantCulture)};" +
+                        $"{logMessage.LogLevel.ToString().PaddingRight(Logger.MaxLengthLogLevel)};{logMessage.Category};" +
+                        $"{logMessage.Message};{logMetricMessage.ValueText};{logMetricMessage.Unit}");
                 }
                 else
                 {
